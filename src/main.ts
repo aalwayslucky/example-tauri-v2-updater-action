65aa7526313576848385b1a9220522e7b196326e
Daniel Ondejka <nopamblefuture@gmail.com>
2023-10-27 19:01:04 +0700

    p

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ let greetMsgEl: HTMLElement | null;
 
 async function greet() {
   if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    // Learrtrstrsn more about Tauri commands at https://tauri.app/v1/guides/features/command
     greetMsgEl.textContent = await invoke("greet", {
       name: greetInputEl.value,
     });
